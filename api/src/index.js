9314179d313fcaab7e47a147f443c54ec07c5f0e
Airton Nicolas Yañez <airtonnicolasy1996@gmail.com>
2022-04-15 16:29:18 -0300

    ultimos cambios antes de deploy back

--- api/src/index.js
+++ api/src/index.js
@@ -2,4 +2,4 @@ import app from "./app";
 //import "./database";
 
 app.listen(process.env.PORT);
-console.log("Server listen in port 3001");
+console.log(`Server listen in port ${process.env.PORT}`);
